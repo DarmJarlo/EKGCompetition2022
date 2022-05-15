5d281a2e35eb04c995d933291c42767960bd40f3
DarmJarlo <jiaxiangyujxy@gmail.com>
2022-05-15 15:44:57 +0200

    idea about processing the data length

--- CNNtrain.py
+++ CNNtrain.py
@@ -1,6 +1,6 @@
 from wettbewerb import load_references
-
-
+import numpy as np
+from matplotlib import pyplot as plt
 '''https://www.youtube.com/watch?v=WvoLTXIjBYU
 https://keras.io/api/layers/pooling_layers/max_pooling2d/
 import tensorflow.keras as keras
@@ -36,8 +36,38 @@ x_train = ecg_leads
 len_leads = []
 for i in range(len(x_train)):
     len_leads.append(len(x_train[i]))
-    print(len_leads)
+max_len = max(len_leads)
+print(max_len)
+count = 0
+x_normal = []
+index_normal = []
+for index in range(len(x_train)):
+    if ecg_labels[index] == 0 and len_leads[index] == 9000:#here this 9000 must be replaced by the number with most valid data later
+        count = count +1
+        x_normal.append(ecg_leads[index])
+        index_normal.append(index)
 
+num_normal = len(x_normal)
 
+#for index in range(len(ecg_normal)):
+ #   x_normal.append(ecg_leads[ecg_normal[index]])
+  #  num_normal.append(len(ecg_leads[_normal[index]]))
 
+print(index_normal)
+print(num_normal)
 
+template_normal = np.sum(x_normal,axis=0).tolist()
+template_normal = [factor/num_normal for factor in template_normal]
+x= range(9000)
+fig= plt.figure(figsize=(15,5))
+plt.plot(x,template_normal,'bo-')
+
+plt.show()
+
+#print(template_normal)
+
+#idea is like because there are different sizes of data.some are 3000 some are even 18000, and because most are with 9000 ,so
+# we use the normal data with length of 9000 to get a template(sum it up) and use this template to pad
+#  the other data with short length and for the long data, we divide it
+# and padd the short part
+#but maybe we should first ignore all the noise data
